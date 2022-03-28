c81d4fb9ea82c55c582d0aceee0c290c454b7719
fahmyfauzi <fahmyfauzii@gmail.com>
2022-03-28 18:36:02 +0700

    fe

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,9 +19,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         User::create([
-            'name' => 'Fahmy Fauzi',
-            'username' => 'fahmyfauzi',
-            'email' => 'fahmyfauzii@gmail.com',
+            'name' => 'Admin',
+            'username' => 'admin',
+            'email' => 'admin@test.test',
             'is_admin' => 1,
             'password' => bcrypt('password')
         ]);
